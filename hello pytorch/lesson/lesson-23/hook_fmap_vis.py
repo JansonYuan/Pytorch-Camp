9681b5868254a8809f9d074f97b023954e804f62
TingsongYu <yts3221@126.com>
2021-01-04 09:08:43 +0800

    增加with no_grad

--- hello pytorch/lesson/lesson-23/hook_fmap_vis.py
+++ hello pytorch/lesson/lesson-23/hook_fmap_vis.py
@@ -13,6 +13,7 @@ import torchvision.utils as vutils
 from torch.utils.tensorboard import SummaryWriter
 import os
 import sys
+import torch
 hello_pytorch_DIR = os.path.abspath(os.path.dirname(__file__)+os.path.sep+".."+os.path.sep+"..")
 sys.path.append(hello_pytorch_DIR)
 
@@ -25,55 +26,56 @@ set_seed(1)  # 设置随机种子
 # flag = 0
 flag = 1
 if flag:
-    writer = SummaryWriter(comment='test_your_comment', filename_suffix="_test_your_filename_suffix")
+    with torch.no_grad():
+        writer = SummaryWriter(comment='test_your_comment', filename_suffix="_test_your_filename_suffix")
 
-    # 数据
-    path_img = "./lena.png"     # your path to image
-    normMean = [0.49139968, 0.48215827, 0.44653124]
-    normStd = [0.24703233, 0.24348505, 0.26158768]
+        # 数据
+        path_img = "./lena.png"     # your path to image
+        normMean = [0.49139968, 0.48215827, 0.44653124]
+        normStd = [0.24703233, 0.24348505, 0.26158768]
 
-    norm_transform = transforms.Normalize(normMean, normStd)
-    img_transforms = transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.ToTensor(),
-        norm_transform
-    ])
+        norm_transform = transforms.Normalize(normMean, normStd)
+        img_transforms = transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),
+            norm_transform
+        ])
 
-    img_pil = Image.open(path_img).convert('RGB')
-    if img_transforms is not None:
-        img_tensor = img_transforms(img_pil)
-    img_tensor.unsqueeze_(0)    # chw --> bchw
+        img_pil = Image.open(path_img).convert('RGB')
+        if img_transforms is not None:
+            img_tensor = img_transforms(img_pil)
+        img_tensor.unsqueeze_(0)    # chw --> bchw
 
-    # 模型
-    alexnet = models.alexnet(pretrained=True)
+        # 模型
+        alexnet = models.alexnet(pretrained=True)
 
-    # 注册hook
-    fmap_dict = dict()
-    for name, sub_module in alexnet.named_modules():
+        # 注册hook
+        fmap_dict = dict()
+        for name, sub_module in alexnet.named_modules():
 
-        if isinstance(sub_module, nn.Conv2d):
-            key_name = str(sub_module.weight.shape)
-            fmap_dict.setdefault(key_name, list())
+            if isinstance(sub_module, nn.Conv2d):
+                key_name = str(sub_module.weight.shape)
+                fmap_dict.setdefault(key_name, list())
 
-            n1, n2 = name.split(".")
+                n1, n2 = name.split(".")
 
-            def hook_func(m, i, o):
-                key_name = str(m.weight.shape)
-                fmap_dict[key_name].append(o)
+                def hook_func(m, i, o):
+                    key_name = str(m.weight.shape)
+                    fmap_dict[key_name].append(o)
 
-            alexnet._modules[n1]._modules[n2].register_forward_hook(hook_func)
+                alexnet._modules[n1]._modules[n2].register_forward_hook(hook_func)
 
-    # forward
-    output = alexnet(img_tensor)
+        # forward
+        output = alexnet(img_tensor)
 
-    # add image
-    for layer_name, fmap_list in fmap_dict.items():
-        fmap = fmap_list[0]
-        fmap.transpose_(0, 1)
+        # add image
+        for layer_name, fmap_list in fmap_dict.items():
+            fmap = fmap_list[0]
+            fmap.transpose_(0, 1)
 
-        nrow = int(np.sqrt(fmap.shape[0]))
-        fmap_grid = vutils.make_grid(fmap, normalize=True, scale_each=True, nrow=nrow)
-        writer.add_image('feature map in {}'.format(layer_name), fmap_grid, global_step=322)
+            nrow = int(np.sqrt(fmap.shape[0]))
+            fmap_grid = vutils.make_grid(fmap, normalize=True, scale_each=True, nrow=nrow)
+            writer.add_image('feature map in {}'.format(layer_name), fmap_grid, global_step=322)
 
 
 
